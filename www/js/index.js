bd3f9fbb755fa8ecaee915fd43ce8a4a8675117c
ryunosuke <ryuryuryu713@gmail.com>
2019-06-27 12:05:31 +0900

    recommend.htmlがほぼ完成

--- www/js/index.js
+++ www/js/index.js
@@ -1,9 +1,25 @@
 // recommend.html
 
 // <ons-card>を量産する処理
-$(window).on('load',function(){
+// $(function () {
+//     // ページの読み込みが完了した後に実行するコード
+//     for (var i = 0; i < 3; i++) {
+//         $('#loop').after('hoge<br>')
+//     }
+// });
+
+
+// ons.getScriptPage().onInit = function () { //ページが初期化された際に発火
+//     this.onShow = $(function () {
+//         // for (var i = 0; i < 3; i++) {
+//             $('#loop').after('hoge<br>')
+//         // }
+//     })
+// };
+
+$(function () {
     // ページの読み込みが完了した後に実行するコード
-    for(var i=0; i<3; i++){
-        $('#loop').after('hoge<br>')
+    for (i = 0; i < 3; i++) {
+        $("#loop").append("hoge<br>")
     }
-});
+})
